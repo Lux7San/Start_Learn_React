995b80172ad8453c24061fa764ab56a26d3dc15b
Ernold123 <ernolds.es@gmail.com>
2021-03-27 11:29:33 +0700

    Jobsheet 2

---
+++ Jobsheet 2/src/Footer.js
@@ -0,0 +1,14 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+
+class Footer extends React.Component{
+    render(){
+        return(
+                <div>
+                    <p>Footer</p>
+                </div>
+        );
+    }
+}
+
+export default Footer;
